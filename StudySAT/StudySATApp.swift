2c9520d4824756566c6e689dc7f7e3fd14f8f660
Oliver Tran <olivertransf@gmail.com>
2025-12-23 10:08:53 -0800

    Initial Commit

---
+++ StudySAT/StudySATApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySATApp.swift
+//  StudySAT
+//
+//  Created by Oliver Tran on 12/23/25.
+//
+
+import SwiftUI
+
+@main
+struct StudySATApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
